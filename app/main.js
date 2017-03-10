af4cf9caf944ce356c25da3d5311eb1dbd52ae15
Itunu Fatoki <itunuworks@gmail.com>
2017-03-10 18:31:29 +0100

    Function findMinMax completed and passed all 4 tests

---
+++ app/main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+module.exports = {
+	findMinMax: function(numberArray){
+		var max = numberArray[0];
+		var min = numberArray[0];
+		var i = 1;
+		
+		while (i<numberArray.length){
+			if (numberArray[i]>max){
+				max = numberArray[i];
+			}
+			if (numberArray[i]<min){
+				min = numberArray[i];
+			}
+			i++;
+		}
+		if (min === max) {
+			return [min];
+		}
+		else{
+			return [min, max];
+		}
+	}
+}
